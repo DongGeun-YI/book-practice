ef56ef3d3b5246ff14d30afac3608fcfb2a02a81
Donggeun <general4star@naver.com>
2015-05-12 23:28:53 +0900

    list view를 리펙터링해서 신규 아이템POST를 처리하도록 함

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -48,6 +48,32 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, '다른 목록 아이템 1')
         self.assertNotContains(response, '다른 목록 아이템 2')
 
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/%d/' % (correct_list.id,),
+            data = {'item_text': '기존 목록에 신규 아이템'}
+        )
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '기존 목록에 신규 아이템')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_POST_redirects_to_list_views(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/' % (correct_list.id,),
+            data = {'item_text': '기존 목록에 신규 아이템'}
+        )
+        self.assertRedirects(response, 'lists/%d/' % (correct_list.id,))
+
+
+
+
     def test_passes_correct_list_to_temlate(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
@@ -55,60 +81,4 @@ class ListViewTest(TestCase):
         self.assertEqual(response.context['list'], correct_list)
 
 
-class NewListTest(TestCase):
-    def test_Saving_a_POST_request(self):
-        self.client.post(
-            '/lists/new',
-            data={'item_text': '신규 작업 아이템'}
-        )
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, '신규 작업 아이템')
-
-    def test_redirects_after_POST(self):
-        response = self.client.post(
-            '/lists/new',
-            data={'item_text': '신규 작업 아이템'}
-        )
-        new_list = List.objects.first()
-        self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
-
-    def test_validation_errors_are_sent_back_to_homepage_template(self):
-        response = self.client.post('/lists/new', data = {'item_text': ''})
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'home.html')
-        expected_error = escape("You can't have an empty item")
-        self.assertContains(response, expected_error)
-
-    def test_invalid_list_items_arent_saved(self):
-        self.client.post('/lists/new', data = {'item_text': ''})
-        self.assertEqual(List.objects.count(), 0)
-        self.assertEqual(Item.objects.count(), 0)
-
-class NewItemTest(TestCase):
-
-    def test_can_save_a_POST_request_to_an_existing_list(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        self.client.post(
-            '/lists/%d/add_item' %(correct_list.id,),
-            data={'item_text': '기존 목록에 신규 아이템'}
-        )
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, '기존 목록에 신규 아이템')
-        self.assertEqual(new_item.list, correct_list)
-
-    def test_redirects_to_list_view(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        response = self.client.post(
-            '/lists/%d/add_item'%(correct_list.id,),
-            data={'item_text': '기존 목록에 신규 아이템'}
-        )
-
-        self.assertRedirects(response, '/lists/%d/' %(correct_list.id))
 
