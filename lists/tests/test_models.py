f12c8dd1a97fb61a5fccd5333eaad9f00aa4d3bb
Donggeun <general4star@naver.com>
2015-05-12 22:55:13 +0900

    모델 검증을 위한 신규목록 뷰 수정

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
 from lists.models import Item, List
-
+from django.core.exceptions import ValidationError
 
 # Create your tests here.
 
-
 class ListAndItemModelsList(TestCase):
+
     def test_saving_and_retrieving_item(self):
         list_ = List()
         list_.save()
@@ -31,4 +31,16 @@ class ListAndItemModelsList(TestCase):
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, '두 번째 아이템')
         self.assertEqual(second_saved_item.list, list_)
+
+    def test_cannot_save_empty_list(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text = '')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean() #SQLite의 부족함으로 인해서, 유효성검증을 수동으로 해주어야함.
+
+
+#with 구문을 try: item.save() \n self.fail('save 기능이 예외를 발생기켜야한다') \except ValidationError: \n pass;
+
+
 
